821b2718d2879e1d03df3c866cbb14dd39a96e77
OolongTea620 <rlafls9596@gmail.com>
2022-12-13 18:11:39 +0900

    BEB-6 ReadOne, update, delete 구현

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,16 +1,5 @@
-const mongoose = require("mongoose");
 const { Product, Test } = require("../schemas");
 const error = require("../middlewares/errorConstructor");
-
-const testInsert = async (req) => {
-  const { name, description } = req.body;
-  try {
-    const result = await Test.create({ name, description });
-    return result;
-  } catch (err) {
-    console.error(err);
-  }
-};
 
 const productInsert = async (req) => {
   const product = req.body;
@@ -31,4 +20,60 @@ const productInsert = async (req) => {
   return result;
 };
 
-module.exports = { testInsert, productInsert };
+const productReadById = async (productId) => {
+  return await Product.findById(productId);
+};
+
+const productReadMany = async (data) => {
+  try {
+    const searchOptions = {};
+    const sortOptions = { createdAt: -1 };
+
+    const result = await Product.find(searchOptions).sort(sortOptions);
+    return result;
+  } catch (err) {
+    throw error(err.message.err.status);
+  }
+};
+
+const productUpdate = async (req) => {
+  const updateData = req.body;
+  const productId = req.body.id;
+  delete updateData.id;
+
+  try {
+    const result = await Product.findByIdAndUpdate(productId, updateData);
+    return result;
+  } catch (err) {
+    throw error(err, err.status);
+  }
+};
+
+const productSoftDelete = async (productId) => {
+  const result = await Product.findByIdAndUpdate(productId, {
+    available: false,
+  });
+  return result;
+};
+
+/**
+ * @description mongoose Test Schema 입력 테스트용
+ */
+const testInsert = async (req) => {
+  const { name, description } = req.body;
+  try {
+    const result = await Test.create({ name, description });
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = {
+  testInsert,
+  productInsert,
+  productReadById,
+  productReadMany,
+  productUpdate,
+  productSoftDelete,
+};
--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -1,6 +1,5 @@
 /**
- *  Product Service 기능 유닛 테스트
- * @unitTest
+ *  @test productService test
  */
 const productService = require("../services/productService");
 const configSetting = require("../../configs");
@@ -12,25 +11,63 @@ beforeAll(() => {
 });
 
 describe("Test Product Service", () => {
-  test("create", async () => {
-    const req = {
-      body: {
-        name: "testprod1",
-        ingredients: {
-          all: ["ingr1", "ingr2", "ingr3"],
-          main: ["ingr1"],
-        },
-        texture: ["하얀 미백색의 제형"],
-        skintype: ["건성", "중성"],
-        recommend_for: ["수분이 필요한 타입"],
-        summary: "중건성을 위한 끈적임 없는 자연 토너",
-        description: "여기는 html text 입력하면 된다.",
+  // input data
+  const req = {
+    body: {
+      name: "testprod1",
+      ingredients: {
+        all: ["ingr1", "ingr2", "ingr3"],
+        main: ["ingr1"],
       },
-    };
+      texture: ["하얀 미백색의 제형"],
+      skintype: ["건성", "중성"],
+      recommend_for: ["수분이 필요한 타입"],
+      summary: "중건성을 위한 끈적임 없는 자연 토너",
+      description: "여기는 html text 입력하면 된다.",
+    },
+  };
 
+  const updateReq = {
+    body: {
+      name: "changeprod1",
+      ingredients: {
+        all: ["ingr1", "ingr2", "ingr4"],
+      },
+      texture: ["하얀 미백색의 제형"],
+      skintype: ["건성"],
+      recommend_for: ["추가적인 수분이 필요한 타입"],
+      description: "여기는 html text 입력하면 된다.",
+    },
+  };
+  let productId = undefined;
+
+  test("create", async () => {
     const result = await productService.productInsert(req);
     expect(result.id).toBeTruthy();
     expect(result.name).toEqual(req.body.name);
+    productId = result.id;
+  });
+
+  test("readById", async () => {
+    const result = await productService.productReadById(productId);
+    expect(result.name).toEqual(req.body.name);
+    expect(result.ingredients.all).toMatchObject(req.body.ingredients.all);
+  });
+
+  test("update", async () => {
+    updateReq.body.id = productId;
+
+    const result = await productService.productUpdate(updateReq);
+    const checkResult = await productService.productReadById(productId);
+    expect(checkResult.id).toEqual(productId);
+    expect(checkResult.name).toEqual(updateReq.body.name);
+  });
+
+  test("delete", async () => {
+    const result = await productService.productSoftDelete(productId);
+    const checkResult = await productService.productReadById(productId);
+    expect(checkResult.id).toEqual(productId);
+    expect(checkResult.available).toBeFalsy();
   });
 });
 
